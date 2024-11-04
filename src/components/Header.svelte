c78ec31aa82d557d614c7552c087d381bf3102bb
rafael <rrrr0ffff@gmail.com>
2024-11-04 05:01:13 -0300

    add header

---
+++ src/components/Header.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+</script>
+
+<header class="bg-orange-600 py-3 px-6 md:px-0 text-3xl">
+	<div class="flex justify-between md:w-2/3 mx-auto">
+		<span class=" text-white font-bold">SvelteMart</span>
+		<div class="relative">
+			<button class="">🛒</button>
+		</div>
+	</div>
+</header>
